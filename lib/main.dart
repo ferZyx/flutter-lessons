d9b72221e43ae2246406ad7dc235b29aa32fa17e
Vladislav Sorokin <sorokin.wlad@yandex.ru>
2024-10-05 16:25:34 +0500

    Индекс массы тела

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,105 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const BMICalculatorApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class BMICalculatorApp extends StatelessWidget {
+  const BMICalculatorApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      home: BMICalculatorScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class BMICalculatorScreen extends StatefulWidget {
+  const BMICalculatorScreen({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<BMICalculatorScreen> createState() => _BMICalculatorScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _BMICalculatorScreenState extends State<BMICalculatorScreen> {
+  final TextEditingController weightController = TextEditingController();
+  final TextEditingController heightController = TextEditingController();
 
-  void _incrementCounter() {
+  String result = "";
+
+  void calculateBMI() {
+    double weight = double.parse(
+        weightController.text);
+    double height =
+        double.parse(heightController.text) / 100;
+
+    double bmi = weight / (height * height);
+
+    String bmiCategory = "";
+    if (bmi < 16) {
+      bmiCategory = "Острый дефицит массы";
+    } else if (bmi >= 16 && bmi <= 18.5) {
+      bmiCategory = "Недостаточная масса тела";
+    } else if (bmi > 18.5 && bmi <= 25) {
+      bmiCategory = "Норма";
+    } else if (bmi > 25 && bmi <= 30) {
+      bmiCategory = "Избыточная масса тела";
+    } else if (bmi > 30 && bmi <= 35) {
+      bmiCategory = "Ожирение первой степени";
+    } else if (bmi > 35 && bmi <= 40) {
+      bmiCategory = "Ожирение второй степени";
+    } else if (bmi > 40) {
+      bmiCategory = "Ожирение третьей степени";
+    }
+
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      result = "Ваш ИМТ: ${bmi.toStringAsFixed(1)} ($bmiCategory)";
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Калькулятор ИМТ'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            TextField(
+              controller: weightController,
+              decoration: const InputDecoration(
+                labelText: 'Вес (кг)',
+                border: OutlineInputBorder(),
+              ),
+              keyboardType: TextInputType.number,
             ),
+            const SizedBox(height: 16),
+
+            TextField(
+              controller: heightController,
+              decoration: const InputDecoration(
+                labelText: 'Рост (см)',
+                border: OutlineInputBorder(),
+              ),
+              keyboardType: TextInputType.number,
+            ),
+            const SizedBox(height: 16),
+
+            ElevatedButton(
+              onPressed: calculateBMI,
+              child: const Text('Рассчитать ИМТ'),
+            ),
+            const SizedBox(height: 16),
+
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              result,
+              style: const TextStyle(fontSize: 20),
+              textAlign: TextAlign.center,
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
