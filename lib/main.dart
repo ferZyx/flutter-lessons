d39d5df8d1ab9af6c465377cd1f90727a05a43d5
Vladislav Sorokin <sorokin.wlad@yandex.ru>
2024-10-05 20:41:13 +0500

    Audio Player

--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,136 @@
-import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
+import 'package:rxdart/rxdart.dart';
 
 void main() {
-  runApp(const DiceApp());
+  runApp(const MyApp());
 }
 
-class DiceApp extends StatelessWidget {
-  const DiceApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: DiceScreen(),
+    return MaterialApp(
+      title: 'Audio Player',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const AudioPlayerScreen(),
     );
   }
 }
 
-class DiceScreen extends StatefulWidget {
-  const DiceScreen({super.key});
+class AudioPlayerScreen extends StatefulWidget {
+  const AudioPlayerScreen({super.key});
 
   @override
-  State<DiceScreen> createState() => _DiceScreenState();
+  State<AudioPlayerScreen> createState() => _AudioPlayerScreenState();
 }
 
-class _DiceScreenState extends State<DiceScreen> {
-  int _diceNumber = 1;
+class _AudioPlayerScreenState extends State<AudioPlayerScreen> {
+  late AudioPlayer _audioPlayer;
+  late Stream<Duration?> _durationStream;
+  late Stream<Duration> _positionStream;
 
-  void _rollDice() {
-    setState(() {
-      _diceNumber = Random().nextInt(6) + 1;
-    });
+  @override
+  void initState() {
+    super.initState();
+    _audioPlayer = AudioPlayer();
+    _audioPlayer.setUrl(
+        'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3');
+    _durationStream = _audioPlayer.durationStream;
+    _positionStream = _audioPlayer.positionStream;
   }
+
+  @override
+  void dispose() {
+    _audioPlayer.dispose();
+    super.dispose();
+  }
+
+  Stream<PositionData> get _positionDataStream =>
+      Rx.combineLatest2<Duration, Duration?, PositionData>(
+        _positionStream,
+        _durationStream,
+        (position, duration) =>
+            PositionData(position, duration ?? Duration.zero),
+      );
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Кости'),
+        title: const Text('Audio Player'),
       ),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Image.asset('assets/dice$_diceNumber.png', height: 150, width: 150),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _rollDice,
-              child: const Text('Кинуть'),
+          children: [
+            StreamBuilder<PositionData>(
+              stream: _positionDataStream,
+              builder: (context, snapshot) {
+                final positionData = snapshot.data;
+                return Column(
+                  children: [
+                    if (positionData != null) ...[
+                      Slider(
+                        min: 0.0,
+                        max: positionData.duration.inMilliseconds.toDouble(),
+                        value: positionData.position.inMilliseconds.toDouble(),
+                        onChanged: (value) {
+                          _audioPlayer
+                              .seek(Duration(milliseconds: value.toInt()));
+                        },
+                      ),
+                      Text(
+                        '${positionData.position.inSeconds} / ${positionData.duration.inSeconds} сек',
+                      ),
+                    ]
+                  ],
+                );
+              },
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    _audioPlayer.seek(Duration.zero);
+                    _audioPlayer.stop();
+                  },
+                  child: const Text('Stop'),
+                ),
+                const SizedBox(width: 20),
+                ElevatedButton(
+                  onPressed: () async {
+                    if (_audioPlayer.playing) {
+                      await _audioPlayer.pause();
+                    } else {
+                      await _audioPlayer.play();
+                    }
+                  },
+                  child: StreamBuilder<bool>(
+                    stream: _audioPlayer.playingStream,
+                    builder: (context, snapshot) {
+                      bool isPlaying = snapshot.data ?? false;
+                      return Text(isPlaying ? 'Pause' : 'Play');
+                    },
+                  ),
+                ),
+              ],
             ),
           ],
         ),
       ),
     );
   }
+}
+
+class PositionData {
+  final Duration position;
+  final Duration duration;
+
+  PositionData(this.position, this.duration);
 }
